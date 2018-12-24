b9b7978b3f228e48893c497559973af52e62f017
David Smith <spiffcow@gmail.com>
2018-12-24 12:20:18 -0800

    data language works!!

--- adt-gen/src/Parser.hs
+++ adt-gen/src/Parser.hs
@@ -1,6 +1,8 @@
 module Parser
-    ( parseNameIdentifier
-    , parseMemberNameIdentifier
+    ( parseDataInfo
+    , parseDataType
+    , parseMemberName
+    , parseDataTypeRef
     ) where
 
 import Control.Applicative
@@ -10,22 +12,51 @@ data Test =  String | Unit
 
 data MemberName = MemberName String deriving (Eq, Ord, Show)
 data MemberType = MemberType String deriving (Eq, Ord, Show)
+data DataTypeName = DataTypeName String deriving (Eq, Ord, Show)
 
 data DataTypeRef = DataTypeRef {
       memberName :: MemberName
     , memberType :: MemberType
     } deriving (Eq, Ord, Show)
 
-data DataTypeExpr
-    = SumExpr [DataTypeRef]        -- data MySumType { a :: A | B | C }
-    | ProductExpr [DataTypeRef]    -- data MyProductType { a :: A, b :: B, ...}
+data DataTypeExpr 
+    = SumExpr [MemberType]             -- data MySumType { A | B | C | ...}
+    | ProductExpr [DataTypeRef]        -- data MyProductType { a :: A, b :: B, ...}
     deriving (Eq, Ord, Show)
 
-parseNameIdentifier :: Parser [Char]
-parseNameIdentifier = do 
+data DataTypeInfoExpr = DataTypeInfoExpr DataTypeName DataTypeExpr deriving (Eq, Ord, Show)
+
+parseMemberIdentifier :: Parser [Char]
+parseMemberIdentifier = do 
     l <- lower 
     s <- many alphaNum
     _ <- spaces
     pure (l : s)
 
-parseMemberNameIdentifier = MemberName <$> parseNameIdentifier
+parseTypeNameIdentifier :: Parser [Char]
+parseTypeNameIdentifier = do 
+    l <- upper 
+    s <- many alphaNum
+    _ <- spaces
+    pure (l : s)
+
+parseMemberName = MemberName <$> parseMemberIdentifier
+parseMemberType = MemberType <$> parseTypeNameIdentifier
+parseDataTypeName = DataTypeName <$> parseTypeNameIdentifier
+
+parseDataTypeRef :: Parser DataTypeRef
+parseDataTypeRef = DataTypeRef <$> parseMemberName <* (symbol "::") <*> parseMemberType
+
+parseProduct :: Parser DataTypeExpr
+parseProduct = ProductExpr <$> parseDataTypeRef `sepBy1` comma
+
+parseSum :: Parser DataTypeExpr
+parseSum = SumExpr <$> parseMemberType `sepBy1` (symbol "|")
+
+parseDataType :: Parser DataTypeExpr
+parseDataType = parseProduct <|> parseSum
+
+parseDataInfo :: Parser DataTypeInfoExpr
+parseDataInfo = DataTypeInfoExpr <$> ((symbol "data") *> parseDataTypeName) <*> braces parseDataType
+    
+    
